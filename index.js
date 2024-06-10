97353907b4984c3da3d11c23f2dcbbef99d877e5
Henrique <henriquealmeidaflorentino61@gmail.com>
2024-06-09 23:55:08 -0300

    Final

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ createApp({
     methods: {
         async fetchAlbums() {
             this.loading = true;
-            const apiKey = '43EC814E3419631DDAFF97CA5FF26BC0'; // 
-            const artist = 'Pink Floyd'; 
+            const apiKey = '4d5269803e6e47e3cb047ab48609f841'; // Sua chave da API do Last.fm
+            const artist = 'Pink Floyd'; // Substitua com o nome do artista que você quer buscar
             const url = `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&api_key=${apiKey}&format=json`;
 
             try {
@@ -38,7 +38,7 @@ createApp({
             }
         },
         async fetchAlbumTracks(album) {
-            const apiKey = '43EC814E3419631DDAFF97CA5FF26BC0'; 
+            const apiKey = '4d5269803e6e47e3cb047ab48609f841'; // Sua chave da API do Last.fm
             const url = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&artist=${album.artist.name}&album=${album.name}&api_key=${apiKey}&format=json`;
 
             try {
